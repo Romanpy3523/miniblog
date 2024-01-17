b1b36a66d6b9b1f2dd458a98bdea3ea260f44879
Romeo <makepy71@gmail.com>
2024-01-17 08:59:52 +0300

    addView

--- mi_blog/blog/views.py
+++ mi_blog/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic.base import View
+from .models import Post
 # Create your views here.
+
+class PostView(View):
+    def get(self, request):
+        posts = Post.objects.all()
+        return render(request, 'blog/index.html', {'post_list': posts})
